cebe84bf0839179209ddb9ee04f57b81d7944b05
Rawpower9 <ritviksetty@gmail.com>
2024-08-25 22:10:45 -0700

    Added starter code to run motor

--- src/main/java/frc/robot/commands/RunMotorCommand.java
+++ src/main/java/frc/robot/commands/RunMotorCommand.java
@@ -4,36 +4,48 @@
 
 package frc.robot.commands;
 
-import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.RunMotorSubsystem;
+
+import java.util.function.Supplier;
+
 import edu.wpi.first.wpilibj2.command.Command;
 
 /** An example command that uses an example subsystem. */
-public class ExampleCommand extends Command {
+public class RunMotorCommand extends Command {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
+  private final RunMotorSubsystem m_subsystem;
+  //We will learn about Suppliers next time!
+  private Supplier<Double> speed;
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public ExampleCommand(ExampleSubsystem subsystem) {
+  public RunMotorCommand(RunMotorSubsystem subsystem, Supplier<Double> speed) {
     m_subsystem = subsystem;
+    this.speed = speed;
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    //TODO: use the `runMotor` method in `RunMotorSubsystem`
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+        m_subsystem.runMotor(0);
+  }
 
   // Returns true when the command should end.
   @Override
---
+++ src/main/java/frc/robot/subsystems/RunMotorSubsystem.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class RunMotorSubsystem extends SubsystemBase {
+
+  //The motor is declared here!
+  private TalonFX motor;
+  private static RunMotorSubsystem instance;
+
+
+  private RunMotorSubsystem() {
+    //TODO: Initalize the motor on port 0
+
+  }
+  // This is a singleton pattern. Ensures only one instance of `RunMotorSubsystem` exists!
+  // See if you can understand how it works!
+  public static RunMotorSubsystem getInstance(){
+    if(instance == null){
+      instance = new RunMotorSubsystem();
+    }
+    return instance;
+  }
+
+  public void runMotor(double speed){
+    //TODO: set motor speed and log speed
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
